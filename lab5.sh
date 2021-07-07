9ff09011e0ee78751ee615ec544b23699afaa929
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-06 17:36:44 -0700

    v3

--- lab5.sh
+++ lab5.sh
@@ -1,24 +1,6 @@
 #!bin/bash
 
 echo --Address Book--
-echo 1. List/Search
-echo 2. Add
-echo 3. Edit
-echo 4. Remove
-echo 5. Quit
-
-
-#echo Enter your selection:
-#read user_choice
-
-
-#choice[1]="List/Search"
-#choice[2]="Add"
-#choice[3]="Edit"
-#choice[4]="Remove"
-#choice[5]="Quit"
-
-#echo "Here is your output, ${choice[user_choice]}"
 
 select user_choice in List/Search, Add, Edit, Remove, Quit
 
