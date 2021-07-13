770e7b7550958fdfb1ab15745e0edb50e7e1b744
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-12 21:40:25 -0700

    d

---
+++ lab6-buggy.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+BOOK="myAddrBook"
+#let the user knnow that this is the address book
+echo "--Address Book--"
+
+#select is used to create a menu efficiently and allows the user to choose an option that is presented before them. 
+echo "Asking the user to select an option"
+select user_choice in List/Search Add Edit Remove Quit
+#select user_choice in List/Search Add Edit Remove Quit
+
+do
+echo "${user_choice}"
+done
+
+echo "If condition starts here"
