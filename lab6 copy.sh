1cd132e9f8e51747765f07d8557c18a33222363a
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-12 21:33:31 -0700

    u

--- lab6 copy.sh
+++ lab6 copy.sh
@@ -10,4 +10,26 @@ select user_choice in List/Search Add Edit Remove Quit
 #Ask the user to select between 5 different options to act on the address book.  a
 
 #check_addrbook ()
-echo "If condition starts here"
+#echo "If condition starts here" 
+
+#if [ ! -f $BOOK ]; then
+#echo "Creating $BOOK..."
+#fi
+
+#if [ ! -f $BOOK ]; then
+#echo "Creating $BOOK ..."
+#touch $BOOK 
+#elif
+#[ ! -r $BOOK ]; then
+#echo "Error: $BOOK not readable"
+#exit 1
+#elif [ ! -w $BOOK ]; then
+#echo "Error: $BOOK not writeable"
+#exit 2
+#fi
+## } 
+
+# check_addrbook
+#do
+#echo "Here is your output, ${user_choice}"
+#done
