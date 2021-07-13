2359be90aab728fae6c4c1924439f5429ee7661f
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-12 18:33:44 -0700

    test

--- lab6.sh
+++ lab6.sh
@@ -6,7 +6,7 @@ echo --Address Book--
 #select is used to create a menu efficiently and allows the user to choose an option that is presented before them. 
 select user_choice in List/Search Add Edit Remove Quit
 
-#Ask the user to select between 5 different options to act on the address book.
+#Ask the user to select between 5 different options to act on the address book.  a
 
 do
 echo "Here is your output, ${user_choice}"
