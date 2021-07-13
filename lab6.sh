252db2ab9aac171173dcb196a27bb140b8f4d6bd
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-12 21:42:26 -0700

    u

--- lab6.sh
+++ lab6.sh
@@ -9,7 +9,6 @@ select user_choice in List/Search Add Edit Remove Quit
 
 do
 echo "Here is your output, ${user_choice}"
-exit 0
 
 
 #Ask the user to select between 5 different options to act on the address book.  a
