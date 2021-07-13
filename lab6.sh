7b618e7687dd5e512caf31b2698e9e9e9883eede
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-12 19:36:11 -0700

    u

--- lab6.sh
+++ lab6.sh
@@ -8,26 +8,26 @@ select user_choice in List/Search Add Edit Remove Quit
 
 #Ask the user to select between 5 different options to act on the address book.  a
 
+function check_addrbook() 
+{
+    if [ ! -f $BOOK ]; then
+    echo "Creating $BOOK ..."
+    touch $BOOK 
+    fi
+    
+    if [ ! -r $BOOK ]; then
+    echo "Error: $BOOK not readable"
+    exit 1
+    fi
+    
+    if [ ! -w $BOOK ]; then
+    echo "Error: $BOOK not writeable"
+    exit 2
+    fi
+}
+
+check_addrbook
 do
 echo "Here is your output, ${user_choice}"
 done
-
-function check_addrbook () {
-
-if [ ! -f $BOOK ]; then
-  echo "Creating $BOOK ..."
-  touch $BOOK 
-fi
-
-if [ ! -r $BOOK ]; then
-  echo "Error: $BOOK not readable"
-  exit 1
-fi
-
-if [ ! -w $BOOK ]; then
-  echo "Error: $BOOK not writeable"
-  exit 2
-fi
-}
-check_addrbook
 
