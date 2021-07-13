2f8983794f001714bffe4847a5aefbaffa7a3853
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-12 21:36:14 -0700

    d

--- lab6.sh
+++ lab6.sh
@@ -5,6 +5,7 @@ echo "--Address Book--"
 
 #select is used to create a menu efficiently and allows the user to choose an option that is presented before them. 
 echo "Asking the user to select an option"
+select user_choice in List/Search Add Edit Remove Quit
 #select user_choice in List/Search Add Edit Remove Quit
 
 echo "If condition starts here"
