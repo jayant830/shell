d1d256848dc34781f0b304e5caa80a6007a36460
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-12 21:28:36 -0700

    d

--- lab6.sh
+++ lab6.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 BOOK="myAddrBook"
 #let the user knnow that this is the address book
-echo --Address Book--
+echo "--Address Book--"
 
 #select is used to create a menu efficiently and allows the user to choose an option that is presented before them. 
 select user_choice in List/Search Add Edit Remove Quit
