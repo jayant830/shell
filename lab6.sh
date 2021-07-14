a0c566370da4bef1ca2078f37abddf77ed938d26
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-13 17:09:47 -0700

    U

--- lab6.sh
+++ lab6.sh
@@ -3,17 +3,7 @@ BOOK="myAddrBook"
 #let the user knnow that this is the address book
 echo "--Address Book--"
 
-#select is used to create a menu efficiently and allows the user to choose an option that is presented before them. 
-echo "Asking the user to select an option"
-select user_choice in List/Search Add Edit Remove Quit
-
-do
-echo "Here is your output, ${user_choice}"
-
-
-#Ask the user to select between 5 different options to act on the address book.  a
-
-#check_addrbook ()
+check_addrbook () {
 
 if [ ! -f $BOOK ]; then
 echo "Creating $BOOK..."
@@ -32,6 +22,17 @@ echo "Error: $BOOK not writeable"
 exit 2
 fi
 } 
+
+#select is used to create a menu efficiently and allows the user to choose an option that is presented before them. 
+echo "Asking the user to select an option"
+select user_choice in List/Search Add Edit Remove Quit
+
+do
+echo "Here is your output, ${user_choice}"
+
+
+#Ask the user to select between 5 different options to act on the address book.  a
+
 
 check_addrbook
 done
