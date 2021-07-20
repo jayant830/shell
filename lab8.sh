85466e6fcd7982e227e214e26580d346e641f05e
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-19 22:56:35 -0700

    u

--- lab8.sh
+++ lab8.sh
@@ -96,6 +96,7 @@ do_edit () {
     echo "What do you want to change it to?"
     read edit_input
     echo $BOOK
+    echo "I want to run SED"
     sed -i 's/$user_input/$edit_input/' $BOOK
     echo "I ran this command"
     cat $BOOK | grep $edit_input
