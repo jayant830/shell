26f1435f736d3b2f4c5fcda12caa19d6fd33778c
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-19 23:06:21 -0700

    backticks

--- lab8.sh
+++ lab8.sh
@@ -97,7 +97,7 @@ do_edit () {
     read edit_input
     echo $BOOK
     echo "I want to run SED"
-    sed -i '/'$user_input'/'$edit_input'/' $BOOK
+    `sed -i '/$user_input/$edit_input/' $BOOK`
     #sed -i 's/$user_input/$edit_input/' $BOOK
     echo "I ran this command"
     cat $BOOK | grep $edit_input
