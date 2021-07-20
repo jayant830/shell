dea31828985845149d0ba7fa987b927d62751515
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-07-19 22:58:33 -0700

    u

--- lab8.sh
+++ lab8.sh
@@ -98,6 +98,7 @@ do_edit () {
     echo $BOOK
     echo "I want to run SED"
     sed -i 's/$user_input/$edit_input/' $BOOK
+    #sed -i 's/$user_input/$edit_input/' $BOOK
     echo "I ran this command"
     cat $BOOK | grep $edit_input
 }
