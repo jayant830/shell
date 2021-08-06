1fbf8c6127a6bb231e071ecfb442f6cba6b3e894
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-08-05 20:45:44 -0700

    d

--- qu1.sh
+++ qu1.sh
@@ -6,38 +6,39 @@ doindent()
 # Depending on your enviroment, you may need to use
 # echo " \c" instead of
 # echo -en " "
-j=0;
-for j in {0...$1}; do
+# j=0;
+for (( j=0; j<=$1; j++ ))
+do
 echo -en " "
 j=`expr $j + 1`
 echo $j
 done
 }
 
-traverse()
-{
+#traverse()
+##{
 # Traverse a directory
-indent="$2"
+#indent="$2"
 
-ls "$1" | for i in {0...$1};
-do
-doindent $2
-if [ -d "$1/$i" ]; then
-echo "Directory: $1/$i"
+#ls "$1" | for i in {0...$1};
+#do
+#doindent $2
+#if [ -d "$1/$i" ]; then
+#echo "Directory: $1/$i"
 # Calling this as a subshell means that when the called
 # function changes directory, it will not affect our
 # current working directory
-traverse "$1/$i" `expr $2 + 1`
-else
-echo "File: $1/$i"
-fi
-done
+#traverse "$1/$i" `expr $2 + 1`
+#else
+#echo "File: $1/$i"
+#fi
+#done
 
-}
+#}
 
-if [ -z "$1" ]; then
-traverse . 0
-else
-traverse "$1" 0
-fi
+#if [ -z "$1" ]; then
+#traverse . 0
+#else
+#traverse "$1" 0
+#fi
 
