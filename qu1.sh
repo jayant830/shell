5e71ac978def42c7b13078dc298dfd84b2cc3fef
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-08-05 20:52:04 -0700

    d

--- qu1.sh
+++ qu1.sh
@@ -11,34 +11,33 @@ for (( j=0; j -lt "$1"; j++ ))
 do
 echo -en " "
 j=`expr $j + 1`
-echo $j
 done
 }
-doindent
-#traverse()
-##{
-# Traverse a directory
-#indent="$2"
 
-#ls "$1" | for i in {0...$1};
-#do
-#doindent $2
-#if [ -d "$1/$i" ]; then
-#echo "Directory: $1/$i"
+traverse()
+{
+# Traverse a directory
+indent="$2"
+
+ls "$1" | for (( i=0; i -lt "$1"; i++ ))
+do
+doindent $2
+if [ -d "$1/$i" ]; then
+echo "Directory: $1/$i"
 # Calling this as a subshell means that when the called
 # function changes directory, it will not affect our
 # current working directory
-#traverse "$1/$i" `expr $2 + 1`
-#else
-#echo "File: $1/$i"
-#fi
-#done
+traverse "$1/$i" `expr $2 + 1`
+else
+echo "File: $1/$i"
+fi
+done
 
-#}
+}
 
-#if [ -z "$1" ]; then
-#traverse . 0
-#else
-#traverse "$1" 0
-#fi
+if [ -z "$1" ]; then
+traverse . 0
+else
+traverse "$1" 0
+fi
 
